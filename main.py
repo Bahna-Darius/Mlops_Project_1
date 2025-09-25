5a6c0a341974a2605477fa0478451ba1fb08d4b9
Bahna-Darius <emanuelbahna@gmail.com>
2025-09-25 19:04:25 +0300

    Main arhitecture

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
-from datascience import logger_app
+from src.datascience.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
+from src.datascience import logger_app
 
 
-logger_app.info("Welcome to my Data Science Project!\n")
 
+STAGE_NAME = "Data Ingestion stage"
+
+
+try:
+   logger_app.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.initiate_data_ingestion()
+   logger_app.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger_app.exception(e)
+        raise e
 
 
