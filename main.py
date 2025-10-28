930741ab1e4f1c51d7d9d7bc44feb1ebf0c9c5ef
Bahna-Darius <emanuelbahna@gmail.com>
2025-10-28 17:59:33 +0200

    Modify file

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from src.datascience.pipeline.data_ingestion_pipeline import DataIngestionTraini
 from src.datascience.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
 from src.datascience.pipeline.data_transformation_pipeline import DataTransformationTrainingPipeline
 from src.datascience.pipeline.model_trainer_pipeline import ModelTrainerTrainingPipeline
+from src.datascience.pipeline.model_evaluation_pipeline import ModelEvaluationTrainingPipeline
 from src.datascience import logger_app
 
 
@@ -57,3 +58,15 @@ except Exception as e:
         logger_app.exception(e)
         raise e
 
+
+STAGE_NAME = "Model evaluation stage"
+
+
+try:
+   logger_app.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   data_ingestion = ModelEvaluationTrainingPipeline()
+   data_ingestion.initiate_model_evaluation()
+   logger_app.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger_app.exception(e)
+        raise e
